45d42a47e73b1ea513a18e4d853173e48620c9d5
usman.khalid3 <usman.khalid3@e732af76-699f-fdf8-a78f-47b5f5a73152>
2010-05-17 12:25:19 +0000

    adding missing GameMenu files

---
+++ Classes/GameMenu.h
@@ -0,0 +1,15 @@
+//
+//  GameMenu.h
+//  NovaRacerBox2d
+//
+//  Created by adeel on 5/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameLayer.h"
+
+@interface GameMenu : CCColorLayer {
+	
+}
+
+@end
